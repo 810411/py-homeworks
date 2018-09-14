998c27038848c88aaaab4ef7d7a44d26e7fa2e73
Grigorii Balabanov <810411@mail.ru>
2018-09-15 00:17:00 +0500

    2.3 done

--- 2.3.formats.json.xml/hw2-3.py
+++ 2.3.formats.json.xml/hw2-3.py
@@ -2,22 +2,22 @@ import json
 import xml.etree.ElementTree as etree
 
 
-def parse_by_key(filename, k):
+def parse_by_key(filename_, k):
     """ Принимает имя файла и слово для ключа, в зависимости от расширения в имени файла парсит соответствующим образом.
     Возвращает список слов из значений ключей(тегов) по переданному слову для ключа"""
-    ext = filename.split('.')[1]
+    ext = filename_.split('.')[1]
     lst = []
     if ext == 'json':
-        with open(filename, encoding='UTF-8') as fl:
+        with open(filename_, encoding='UTF-8') as fl:
             from_json = json.load(fl)
             search_recursive_and_split(from_json, k, lst)
     if ext == 'xml':
-        tree = etree.parse(filename.encode('cp1251'))
+        tree = etree.parse(filename_.encode('cp1251'))
         from_xml = tree.findall(f'.//{k}')
         for item in from_xml:
             lst += item.text.split()
     if ext == 'txt':
-        with open(filename, encoding='UTF-8') as fl:
+        with open(filename_, encoding='UTF-8') as fl:
             for line in fl.readlines():
                 lst += line.split()
     return lst
@@ -36,13 +36,13 @@ def search_recursive_and_split(obj, k, lst):
             search_recursive_and_split(item, k, lst)
 
 
-def extract_top_ten(lst):
+def extract_top_ten(list_for_extraction):
     """ Выбираем из списка слова длиннее 6 букв, сортируем по длине слов, выводим первые десять неповоторяющихся слов"""
-    lst = [word for word in lst if len(word) > 6]
-    lst = sorted(lst, reverse=True, key=lambda x: lst.count(x))
+    list_for_extraction = [word for word in list_for_extraction if len(word) > 6]
+    list_for_extraction = sorted(list_for_extraction, reverse=True, key=lambda x: list_for_extraction.count(x))
     counter = 1
     top_ten = []
-    for word in lst:
+    for word in list_for_extraction:
         if word not in top_ten:
             top_ten.append(word)
             print(f'{counter}. {word}')
@@ -52,8 +52,9 @@ def extract_top_ten(lst):
     return top_ten
 
 
-filenames = ['files/newsafr.txt', 'files/newsafr.json', 'files/newsafr.xml']
-for filename in filenames:
+file_names = ['newsafr.txt', 'newsafr.json', 'newsafr.xml']
+for filename in file_names:
     print(f'Топ 10 самых часто встречающихся в новостях слов длиннее 6 символов для {filename}')
-    lst = parse_by_key(filename, 'title') + parse_by_key(filename, 'description')
-    extract_top_ten(lst)
+    top_ten_list = parse_by_key(f'files/{filename}', 'title') + (
+        parse_by_key(f'files/{filename}', 'description') if filename.split('.')[1] != 'txt' else [])
+    extract_top_ten(top_ten_list)
