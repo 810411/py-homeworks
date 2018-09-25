4965b819bc636219f81655c983ee119c44847d21
Grigorii Balabanov <810411@mail.ru>
2018-09-25 20:56:54 +0500

    3.2 done

---
+++ 3.2.classes.vk/create_token_url.py
@@ -0,0 +1,18 @@
+from urllib.parse import urlencode
+
+
+def create_token(app_id):
+    """
+    Создает ссылку для получения ключа доступа token
+    :param app_id: <str> ID Вашего приложения ВК
+    """
+    oauth_data = {
+        'client_id': app_id,
+        'display': 'page',
+        'scope': 'status',
+        'response_type': 'token'
+    }
+    print('?'.join(('https://oauth.vk.com/authorize', urlencode(oauth_data))))
+
+
+create_token('') #  Заполнить id своего приложения VK
---
+++ 3.2.classes.vk/hw3-2.py
@@ -0,0 +1,74 @@
+# from urllib.parse import urlencode
+import requests
+import time
+
+TOKEN = ''  # Заполнить значением ключа доступа ВК
+
+
+class UserVK:
+    """ Класс для экземпляров описывающих пользователей VK и реализующий поиск общих друзей экземпляров
+    Экземпляр класса создается на основе идентификатора пользователя VK"""
+
+    def __init__(self, ids, token=TOKEN):
+        """
+        :param ids: <str> идентификатор пользователя VK
+        :param token: <str> ключа доступа VK
+        """
+        self.ids = ids
+        self.main_info = self.main_info()
+        self.id = self.main_info['id']
+
+    def __str__(self):
+        return f'https://vk.com/id{self.ids}'
+
+    def __and__(self, other):
+        friend_list = set(self.friends) & set(other.friends)
+        users_list = []
+        for user_id in friend_list:
+            time.sleep(0.5)
+            users_list.append(UserVK(user_id))
+        return users_list
+
+    @property
+    def params(self):
+        return {
+            'access_token': TOKEN,
+            'v': '5.85',
+        }
+
+    def main_info(self):
+        params = self.params
+        params['user_ids'] = self.ids
+        response = requests.get(f'https://api.vk.com/method/users.get', params).json()
+        if 'error' in response:
+            print(response['error']['error_msg'])
+            exit(1)
+        return response['response'][0]
+
+    @property
+    def friends(self):
+        lst = []
+        params = self.params
+        params['user_id'] = self.id
+        response = requests.get(f'https://api.vk.com/method/friends.get', params).json()
+        if 'response' in response:
+            lst = response['response']['items']
+        return lst
+
+    @property
+    def info(self):
+        first_name = self.main_info['first_name']
+        last_name = self.main_info['last_name']
+        return f'Пользователь: {first_name} {last_name}.'
+
+
+# Создаем двух пользователей, выводим их Имя Фамилию и ссылку на страницу, сравниваем списки друзей, добавляем
+# общих друзей в список, выводим Имя Фамилию и ссылку на страницу для общих друзей
+user1 = UserVK('28234227')
+print(user1.info, user1)
+user2 = UserVK('27404809')
+print(user2.info, user2)
+print('Список общих друзей (построение займет какое-то время в зависимости от количества результатов):')
+common_friends = user1 & user2
+for friend in common_friends:
+    print(friend.info, friend)
